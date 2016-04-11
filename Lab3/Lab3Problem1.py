0e64e2bc1148a1a939d1e6115edda79afd6e2801
Will Ashman <Will@Wills-MacBook-Pro.local>
2016-04-11 11:47:07 -0400

    Lab3 Homework

---
+++ Lab3/Lab3Problem1.py
@@ -0,0 +1,78 @@
+#Project Name: Lab 3 Problem 1
+#Date: 4/13/16
+#Programmer Name: Will Ashman
+#Project Description: Program to award points based on the amount of books purchased in a month
+
+def main():
+    #Takes user input and converts to float
+    booksPurchased = float(input('Number of books purchased this month: '))
+    print()
+
+    #Checks if input is greater than or equal to 0
+    if booksPurchased >= 0:
+        simpleIfFunction(booksPurchased)
+        nestedIfFunction(booksPurchased)
+        elifFunction(booksPurchased)
+    else:
+        print('Invalid Entry')
+
+    print()
+    input('Press Enter to continue...')
+
+#Checks value of books purchased to determine the number of points awarded using standard if statements
+def simpleIfFunction(booksPurchased):
+    #Default number of points awarded
+    pointsAwarded = 0
+
+    if booksPurchased == 1:
+        pointsAwarded = 5
+
+    if booksPurchased == 2:
+        pointsAwarded = 15
+
+    if booksPurchased == 3:
+        pointsAwarded = 30
+
+    if booksPurchased > 3:
+        pointsAwarded = 60
+
+    #Prints number of points awarded to the console
+    print('The simpleIfFunction awarded', pointsAwarded, 'points.')
+
+#Uses nested conditionals to determine number of points awarded
+def nestedIfFunction(booksPurchased):
+    #Default number of points awarded
+    pointsAwarded = 0
+
+    if booksPurchased == 1:
+        pointsAwarded = 5
+    else:
+        if booksPurchased == 2:
+            pointsAwarded = 15
+        else:
+            if booksPurchased == 3:
+                pointsAwarded = 30
+            else:
+                if booksPurchased > 3:
+                    pointsAwarded = 60
+
+    #Prints number of points awarded to the console
+    print('The nestedIfFunction awarded', pointsAwarded, 'points.')
+
+#Uses elif to determine number of points awarded
+def elifFunction(booksPurchased):
+    #Default number of points awarded
+    pointsAwarded = 0
+
+    if booksPurchased == 1:
+        pointsAwarded = 5
+    elif booksPurchased == 2:
+        pointsAwarded = 15
+    elif booksPurchased == 3:
+        pointsAwarded = 30
+    elif booksPurchased > 3:
+        pointsAwarded = 60
+
+    print('The elifFunction awarded', pointsAwarded, 'points.')
+
+main()
---
+++ Lab3/Lab3Problem2.py
@@ -0,0 +1,36 @@
+#Project Name: Lab 3 Problem 2
+#Date: 4/13/16
+#Programmer Name: Will Ashman
+#Project Description: Program to determine if a number is divisible via logical operators
+
+def main():
+    #Gathers user input and converts to integer
+    number = int(input('Enter a number: '))
+    print()
+
+    #Passes integer into determination functions
+    determineAnd(number)
+    determineOR(number)
+
+    print()
+    input('Press Enter to continue...')
+
+#Checks if the input integer is divisible by 5 AND 6 using the logical and operator
+def determineAnd(number):
+    if isDivisible(number, 5) and isDivisible(number, 6):
+        print(number, 'is divisible by 5 and 6.')
+    else:
+        print(number, 'is not divisible by 5 and 6.')
+
+#Checks if the input integer is divisble by 5 OR 6 using the logical or operator
+def determineOR(number):
+    if isDivisible(number, 5) or isDivisible(number, 6):
+        print(number, 'is divisible by 5 or 6.')
+    else:
+        print(number, 'is not divisible by 5 or 6.')
+
+#Helper function using modulo to determine if there is no remainder, therefore completely divisible
+def isDivisible(dividend, divisor):
+    return (dividend % divisor) == 0
+
+main()
